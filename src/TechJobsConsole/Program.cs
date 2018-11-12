4966533f1ae172c1e1485f89b373407b6ea1e8a1
Adrian Garcia <aogbgeo@gmail.com>
2018-11-11 21:56:11 -0500

    print jobs done

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -118,7 +118,15 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            foreach(var job in someJobs)
+            {
+
+                foreach (var dict in job)
+                {
+                    Console.WriteLine(dict.Key + ": " + dict.Value);
+                }
+            }
+           
         }
     }
 }
